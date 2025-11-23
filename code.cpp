93d929a2243d115193ca5f93f310ea7acd37c46c
PlazmaBot2000 <guardian9641@gmail.com>
2025-11-23 14:24:06 +0300

    v0.5.1 Update example

--- code.cpp
+++ code.cpp
@@ -4,18 +4,15 @@
 #include <SDL2/SDL_image.h>
 #include <bits/stdc++.h>
 
-int Last_Use = 0;
 SDL_Texture* block;
 int x, y, dx, dy;
-int wwidth, wheight;
+float speed = 2;
 
 void NextPos(SDL_Window *window){
-	if((x >= (ISOEngine_GetWindowWidth(window) - 224)) or (x <= 0))
-		dx = -dx;
-	if((y >= (ISOEngine_GetWindowHeight(window) - 105)) or (y <= 0))
-		dy = -dy;
-	x += dx;
-	y += dy;
+	dx = ISOEngine_GetAxis::x();
+	dy = ISOEngine_GetAxis::y();
+	x += dx * speed;
+	y += dy * speed;
 }
 
 void start(SDL_Window *window, SDL_Renderer *renderer){
@@ -23,8 +20,8 @@ void start(SDL_Window *window, SDL_Renderer *renderer){
 	RenderTexture(block, renderer, x, y);
 	x = ISOEngine_Random(0, 800);
     y = ISOEngine_Random(0, 800);
-	dx = 2;
-	dy = 2;
+	dx = 0;
+	dy = 0;
 }
 
 void loop(SDL_Window *window, SDL_Renderer *renderer){
